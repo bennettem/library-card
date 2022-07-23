06009d7d165d755f241e63a7f9b0831706ab7744
bennettem <emily.pamelabennett@gmail.com>
2022-07-22 21:12:33 -0500

    login form changed

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,15 +28,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const{data} = await login({variables: {...userFormData}})
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
